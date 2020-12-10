4916e1db2633c60990b199fe6218bbc9599b048e
Yangyang Fu <yangyang.fu@colorado.edu>
2020-12-10 16:05:17 -0600

    Add pytorch docker with GPU capabilities in docker

---
+++ pytorch/Python3/run.sh
@@ -0,0 +1,14 @@
+exec docker run \
+ 	  --name gym_test \
+      --user=root \
+	  --detach=false \
+	  -e DISPLAY=${DISPLAY} \
+	  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+	  --rm \
+	  -v `pwd`:/mnt/shared \
+	  -i \
+      -t \
+	  yangyangfu/pytorch_py3 /bin/bash -c "cd /mnt/shared && python /mnt/shared/test_torch.py"
+
+exit $
+  
