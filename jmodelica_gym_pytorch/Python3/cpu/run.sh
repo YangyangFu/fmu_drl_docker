bbfde9f256a6e84bfcf1c0ba59ab6de6defc47c6
Yangyang Fu <yangyang.fu@colorado.edu>
2021-06-06 01:46:31 -0500

    Added docker file for pytorch

---
+++ jmodelica_gym_pytorch/Python3/cpu/run.sh
@@ -0,0 +1,14 @@
+exec docker run \
+ 	  --name test \
+      --user=root \
+	  --detach=false \
+	  -e DISPLAY=${DISPLAY} \
+	  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+	  --rm \
+	  -v `pwd`:/mnt/shared \
+	  -i \
+      -t \
+	  yangyangfu/modelicagym_py3_pytorch_cpu /bin/bash -c "cd /mnt/shared && python /mnt/shared/test_torch.py"
+      
+exit $
+  
